31bc75c70635528311de1f0c165f8ee782459c6e
costagavras <cgavras@yahoo.ca>
2018-09-25 22:35:08 -0400

    edit function added

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -11,11 +11,41 @@ class PicturesController < ApplicationController
     @picture = Picture.new
   end
 
-  def create
+  def edit
+    @picture = Picture.find(params[:id])
   end
 
   def create
-  render text: "Received POST request to '/pictures' with the data URL: #{params}"
-end
 
+  @picture = Picture.new
+
+  @picture.title = params[:picture][:title]
+  @picture.artist = params[:picture][:artist]
+  @picture.url = params[:picture][:url]
+
+
+    if @picture.save
+      # if the picture gets saved, generate a get request to "/pictures" (the index)
+      redirect_to "/pictures"
+    else
+      # otherwise render new.html.erb
+      render :new
+    end
+  end
+
+  def update
+
+  @picture = Picture.find(params[:id])
+
+  @picture.title = params[:picture][:title]
+  @picture.artist = params[:picture][:artist]
+  @picture.url = params[:picture][:url]
+
+
+    if @picture.save
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      render :edit
+    end
+  end
 end
