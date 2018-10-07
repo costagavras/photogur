b42efe65211ca4fe2a5273ce814c710be21dc593
costagavras <cgavras@yahoo.ca>
2018-10-07 00:01:09 -0400

    authorizations working, final

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,13 +1,20 @@
 class PicturesController < ApplicationController
+
   before_action :ensure_logged_in, except: [:show, :index]
+  before_action :load_picture, only: [:show, :edit, :update, :destroy]
+  before_action :ensure_user_owns_picture, only: [:edit, :update, :destroy]
+
   def index
     @pictures = Picture.all
     @age_pictures_desc = Picture.oldest_last
     # @created_before = Picture.created_before(time)
   end
 
-  def show
+  def load_picture
     @picture = Picture.find(params[:id])
+  end
+
+  def show
   end
 
   def new
@@ -15,17 +22,16 @@ class PicturesController < ApplicationController
   end
 
   def edit
-    @picture = Picture.find(params[:id])
   end
 
   def create
 
-  @picture = Picture.new
+    @picture = Picture.new
 
-  @picture.title = params[:picture][:title]
-  @picture.artist = params[:picture][:artist]
-  @picture.url = params[:picture][:url]
-
+    @picture.title = params[:picture][:title]
+    @picture.artist = params[:picture][:artist]
+    @picture.url = params[:picture][:url]
+    @picture.user_id = @current_user.id #or session[:user_id]
 
     if @picture.save
       # if the picture gets saved, generate a get request to "/pictures" (the index)
@@ -37,13 +43,10 @@ class PicturesController < ApplicationController
   end
 
   def update
-
-  @picture = Picture.find(params[:id])
-
-  @picture.title = params[:picture][:title]
-  @picture.artist = params[:picture][:artist]
-  @picture.url = params[:picture][:url]
-  @picture.created_at = params[:picture][:created_at]
+    @picture.title = params[:picture][:title]
+    @picture.artist = params[:picture][:artist]
+    @picture.url = params[:picture][:url]
+    @picture.created_at = params[:picture][:created_at]
 
     if @picture.save
       redirect_to "/pictures/#{@picture.id}"
@@ -53,9 +56,15 @@ class PicturesController < ApplicationController
   end
 
   def destroy
-    @picture = Picture.find(params[:id])
     @picture.destroy
     redirect_to "/pictures"
+  end
+
+  def ensure_user_owns_picture
+    unless current_user == @picture.user
+      flash[:alert] = "Please log in"
+      redirect_to new_session_url
+    end
   end
 
 end
